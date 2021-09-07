eb480d52802cc5e9606e512d07a9d5cd06c172b0
Ermax81 <enozay.dev@gmail.com>
2021-09-07 14:36:06 +0200

    Add checkbox and insert image in textfield

--- src/test/java/com/example/demospringbootpdfbox/FirstExampleTest.java
+++ src/test/java/com/example/demospringbootpdfbox/FirstExampleTest.java
@@ -1,14 +1,23 @@
 package com.example.demospringbootpdfbox;
 
+import org.apache.pdfbox.cos.COSArray;
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.io.MemoryUsageSetting;
+import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDCheckBox;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.Objects;
 
 public class FirstExampleTest {
 
@@ -16,21 +25,59 @@ public class FirstExampleTest {
     void updatePdf() {
         String fileName = "tmp.pdf";
         String outputFileName = "pdf-java-output.pdf";
-        File file = new File(getClass().getClassLoader().getResource(fileName).getFile());
-        if (file == null) {
-            throw new IllegalArgumentException("file not found! " + fileName);
-        }
+        String imageFileName = "image_600x600.jpg";
+
+        File file = new File(Objects.requireNonNull(getClass().getClassLoader().getResource(fileName)).getFile());
 
         try {
             PDDocument pDDocument = PDDocument.load(file);
             PDAcroForm pDAcroForm = pDDocument.getDocumentCatalog().getAcroForm();
+
+            // text_1
+            // insert text in TextField
             PDField field = pDAcroForm.getField("text_1");
             if (field==null) {
                 System.out.println("Field not found");
+            } else {
+                field.setValue("This is a first field printed by Java");
             }
-            field.setValue("This is a first field printed by Java");
+
+            // text_2
+            // insert text in TextField
             field = pDAcroForm.getField("text_2");
-            field.setValue("This is a second field printed by Java");
+            if (field!=null) {
+                field.setValue("This is a second field printed by Java");
+            }
+
+            // checkbox_1
+            // check checkbox
+            // Source: https://stackoverflow.com/questions/14602821/how-to-check-a-check-box-in-pdf-form-using-java-pdfbox-api
+            field = pDAcroForm.getField("checkbox_1");
+            if (field!=null) {
+                ((PDCheckBox) field).check();
+            }
+
+            // picto_1
+            // insert image in textfield 'area'
+            field = pDAcroForm.getField("picto_1");
+            PDImageXObject pdImage = PDImageXObject.createFromFile(Objects.requireNonNull(getClass().getClassLoader().getResource(imageFileName)).getPath(), pDDocument);
+            if (field!=null) {
+                // source: https://stackoverflow.com/questions/46799087/how-to-insert-image-programmatically-in-to-acroform-field-using-java-pdfbox
+
+                // getFieldArea (PDRectangle)
+                COSDictionary fieldDict = field.getCOSObject();
+                COSArray fieldAreaArray = (COSArray) fieldDict.getDictionaryObject(COSName.RECT);
+
+                PDRectangle rectangle = new PDRectangle(fieldAreaArray);
+                float size = rectangle.getHeight();
+                float x = rectangle.getLowerLeftX();
+                float y = rectangle.getLowerLeftY();
+
+                try (PDPageContentStream contentStream = new PDPageContentStream(pDDocument,
+                        pDDocument.getPage(0), PDPageContentStream.AppendMode.APPEND, true)) {
+                    contentStream.drawImage(pdImage, x, y, size, size);
+                }
+            }
 
             pDDocument.save(new File("src/test/resources/"+outputFileName));
             pDDocument.close();
@@ -38,5 +85,26 @@ public class FirstExampleTest {
             e.printStackTrace();
         }
 
+    }
+
+    @Test
+    void mergePdf() throws FileNotFoundException {
+        String fileOneName = "tmp.pdf";
+        String fileTwoName = "pdf-java-output.pdf";
+        String fileOutputName = "pdf-merged.pdf";
+
+        // Merge 2 pdf files
+        PDFMergerUtility PDFmerger = new PDFMergerUtility();
+        PDFmerger.setDestinationFileName("src/test/resources/"+fileOutputName);
+        PDFmerger.addSource(new File("src/test/resources/"+fileOneName));
+        PDFmerger.addSource(new File("src/test/resources/"+fileTwoName));
+
+        MemoryUsageSetting inMemory = MemoryUsageSetting.setupMainMemoryOnly();
+        //MemoryUsageSetting tmpFile = MemoryUsageSetting.setupTempFileOnly();
+        try {
+            PDFmerger.mergeDocuments(inMemory);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
